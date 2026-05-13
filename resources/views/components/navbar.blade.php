7de393346f82e6e2806466b0fb5ddecc66e82189
DEV-BHAVSAR0077 <dev.bhavsar0077@example.com>
2026-05-13 09:27:25 +0530

    Change the Tab name employee to Users

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -11,7 +11,7 @@
         @can('view-user')
         <a href="{{ route('dashboard', ['tab' => 'emp']) }}" class="nav-tab-btn {{ $empTabActive ? 'active' : '' }}" id="tab-btn-emp">
             <svg width="14" height="14" viewBox="0 0 20 20" fill="currentColor"><path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.660.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"/></svg>
-            Employees
+            Users
         </a>
         @endcan
         @can('view-role')
