75a41375a3f910a73d4887c67b67045830d10623
DEV-BHAVSAR0077 <dev.bhavsar0077@example.com>
2026-05-26 09:04:26 +0530

    update the minor changes

--- app/Services/SyncBalance.php
+++ app/Services/SyncBalance.php
@@ -8,11 +8,8 @@ class SyncBalance
 {
     public static function updateBalance(?int $agencyVendorId, float $amount, string $type, string $action, ?int $paymentType = null)
     {
-        if (!$agencyVendorId) {
-            return 0;
-        }
-
         $vendor = AgencyVendor::find($agencyVendorId);
+
         if (!$vendor) {
             return 0;
         }
